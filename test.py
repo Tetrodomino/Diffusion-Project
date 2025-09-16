5c5f0b0daea16fb0e011c94b162b34779e826bfa
Tetrodomino <wettrimotto@naver.com>
2025-09-16 17:01:31 +0900

    2025-09-16 prepare

--- test.py
+++ test.py
@@ -1,1 +1,10 @@
 print('test')
+
+# 가상환경에 라이브러리 설치
+# pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu121
+# pip install diffusers transformers accelerate safetensors
+
+# 사용 전
+# python -m venv venv
+# source venv/bin/activate (가상환경 실행)
+# deactivate (가상환경 종료)
